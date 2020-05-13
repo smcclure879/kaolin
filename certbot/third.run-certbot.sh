c1733504da18f143ba5c20750544da02ae1e9b1d
root <ayvex llc>
2020-05-13 10:15:43 +0000

    fix third to point not at second 	modified:   certbot/third.run-certbot.sh

--- certbot/third.run-certbot.sh
+++ certbot/third.run-certbot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/certbot-auto certonly --webroot --preferred-challenges http --domains third.ayvexllc.com --webroot-path /root/gitstuff/kaolin/www-second
+/usr/local/bin/certbot-auto certonly --webroot --preferred-challenges http --domains third.ayvexllc.com --webroot-path /root/gitstuff/kaolin/www-third
